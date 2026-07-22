e8a1c1563e78f95cce816605b5afd75aac059384
Enoch <enochchiusi@astri.org>
2026-07-22 17:01:46 +0800

    format: reformat vite.config.js for improved readability

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,16 @@
-import{defineConfig}from'vite';import react from'@vitejs/plugin-react';export default defineConfig({plugins:[react()],server:{proxy:{'/api':'http://127.0.0.1:3001'}}});
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+
+  // GitHub repository name, including leading/trailing slash
+  base: '/Bible-Reader/',
+
+  server: {
+    proxy: {
+      '/api': 'http://127.0.0.1:3001',
+    },
+  },
+})
